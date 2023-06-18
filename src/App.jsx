fb594018d4655011800284dd609cf4452e0c7775
Henrique Queiroz de Paula <henrique.q.paula@gmail.com>
2023-06-18 13:34:21 -0300

    [Feat]

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
@@ -18,11 +20,46 @@ const App = () => {
     },
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ... tasks, 
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  }
+
+  const handleTaskClick = (taskid) => {
+    const newTasks = tasks.map((task) => {
+      if(task.id === taskid){
+        return {
+          ... task,
+          completed: !task.completed
+        }
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  const handleTaskDeletion = (taskid) => {
+    const newTasks = tasks.filter(task => task.id !== taskid);
+
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <div className="container">
-        <AddTask />
-        <Tasks tasks={tasks} />
+        <Header />
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
       </div>
     </>
   );
